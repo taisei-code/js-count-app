5412777557c8f5b5c6962e7bef1d2d7919d3ddc7
Taisei <taisei96.web@gmail.com>
2023-04-28 09:46:32 +0900

    リファクタリング

--- js/main.js
+++ js/main.js
@@ -7,18 +7,16 @@ const countNum  = document.querySelector('#count-num');
 // ローカルストレージにカウントを格納する配列
 let countData = [];
 
+
+// カウントを増やすボタンをクリックしたときのイベント
 countPush.addEventListener('click', () => {
   countUp();
-  saveLS();
+  // ローカルストレージに配列に格納されたデータを保存
+  localStorage.setItem('数字', JSON.stringify(countData))
 })
 
 // カウントを増やす処理の関数
 function countUp() {
   let countDatum = countNum.textContent++;
   countData.push(countDatum);
-}
-
-// ローカルストレージに配列に格納されたデータを保存する関数
-function saveLS() {
-  localStorage.setItem('数字', JSON.stringify(countData))
 }
