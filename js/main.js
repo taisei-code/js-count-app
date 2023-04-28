91d8a9281a0048ec96902397fe6361a1d1ab0373
Taisei <taisei96.web@gmail.com>
2023-04-28 09:32:44 +0900

    README記入

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 'use strict'
 
+// カウントを増やすボタン
 const countPush = document.querySelector('#count-push');
+//　カウントを表示する要素
 const countNum  = document.querySelector('#count-num');
-
+// ローカルストレージにカウントを格納する配列
 let countData = [];
 
 countPush.addEventListener('click', () => {
@@ -10,11 +12,13 @@ countPush.addEventListener('click', () => {
   saveLS();
 })
 
+// カウントを増やす処理の関数
 function countUp() {
   let countDatum = countNum.textContent++;
   countData.push(countDatum);
 }
 
+// ローカルストレージに配列に格納されたデータを保存する関数
 function saveLS() {
   localStorage.setItem('数字', JSON.stringify(countData))
 }
